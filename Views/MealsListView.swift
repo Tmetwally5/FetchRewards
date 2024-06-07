f40d36025c309c8f8d14b944c1c326bdb321151b
Taha Metwally <tmetwally5@gmail.com>
2024-06-06 23:58:00 -0500

    enhancement in the design in mealslistview

--- Views/MealsListView.swift
+++ Views/MealsListView.swift
@@ -9,21 +9,32 @@ import SwiftUI
 
 struct MealsListView: View {
     @ObservedObject var viewModel: MealsViewModel
-    @State private var searchText: String = ""
+    @ObservedObject var detailsViewModel: MealDetailViewModel
+    @State private var searchQuery: String = ""
+    @State private var selectedSearchOption: SearchOption = .category
     
     init(networkService: NetworkService) {
         self.viewModel = MealsViewModel(networkService: networkService)
+        self.detailsViewModel = MealDetailViewModel(networkService: networkService)
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                TextField("Enter category", text: $searchText)
+                Picker("Search By", selection: $selectedSearchOption) {
+                    ForEach(SearchOption.allCases, id: \.self) { option in
+                        Text(option.rawValue.capitalized)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                TextField("Enter \(selectedSearchOption.rawValue.capitalized)", text: $searchQuery)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
                 
                 Button(action: {
-                    viewModel.fetchMeals(category: searchText)
+                    viewModel.fetchMeals(category: searchQuery)
                 }) {
                     Text("Search")
                         .foregroundColor(.white)
@@ -39,11 +50,26 @@ struct MealsListView: View {
                 }
                 
                 List(viewModel.meals) { meal in
-                    Text(meal.strMeal ?? "")
+                    NavigationLink(destination: MealDetailView(viewModel: detailsViewModel, mealId: meal.id ?? "")) {
+                        Text(meal.strMeal ?? "")
+                    }
                 }
             }
             .padding()
             .navigationTitle("Recipes")
         }
+    }
+}
+
+enum SearchOption: String, CaseIterable {
+    case category
+    case area
+    case ingredient
+    case name
+}
+
+struct MealsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealsListView(networkService: NetworkService())
     }
 }
