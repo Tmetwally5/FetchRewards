d734af63145a598d1505be115ed74f24e9c9af14
Taha Metwally <tmetwally5@gmail.com>
2024-06-06 21:34:46 -0500

    create mealviewmodel and mealdetailsviewmodel

--- ViewModels/MealDetailViewModel.swift
+++ ViewModels/MealDetailViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by Taha Metwally on 6/6/2024.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class MealDetailViewModel: ObservableObject {
+    
+    @Published var meal: MealDetails? = nil
+    @Published var errorMessage: String? = nil
+    private var cancellables = Set<AnyCancellable>()
+    private var networkService:NetworkService
+    
+    init(networkService:NetworkService) {
+        self.networkService = networkService
+    }
+    
+    func fetchMealDetail(byId id: String) {
+        networkService.fetchMealDetailsByID(meal_ID: id)
+            .receive(on: DispatchQueue.global())
+            .handleEvents(receiveSubscription: {[weak self] _ in
+                self?.errorMessage = nil // Clear previous errors
+            })
+            .sink(receiveCompletion: {[weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.errorMessage = "Failed to fetch meal detail: \(error.localizedDescription)"
+                }
+            }, receiveValue: {[weak self] response in
+                self?.meal = response.meals.first
+            })
+            .store(in: &cancellables)
+    }
+}
+
+
--- ViewModels/MealsViewModel.swift
+++ ViewModels/MealsViewModel.swift
@@ -5,4 +5,34 @@
 //  Created by Taha Metwally on 6/6/2024.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class MealsViewModel: ObservableObject {
+    
+    @Published var meals: [Meal] = []
+    private var cancellables = Set<AnyCancellable>()
+    private var networkService:NetworkService
+    @Published var errorMessage: String? = nil
+    
+    init(networkService:NetworkService) {
+        self.networkService = networkService
+    }
+
+    func fetchMeals(category:String) {
+        networkService.fetchMealListByCategory(category: category)
+            .receive(on: DispatchQueue.global())
+            .handleEvents(receiveSubscription: {[weak self] _ in
+                self?.errorMessage = nil // Clear previous errors
+            })
+            .sink(receiveCompletion: {[weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.errorMessage = "Failed to fetch meal detail: \(error.localizedDescription)"
+                }
+            }, receiveValue: {[weak self] response in
+                self?.meals = response.meals
+                                   .compactMap { $0 } // Ensure meals are non-nil
+                                   .sorted { ($0.strMeal ?? "") < ($1.strMeal ?? "") }
+            }).store(in: &cancellables)
+    }
+}
