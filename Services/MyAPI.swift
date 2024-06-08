e1b9d5193c2f2caa19197c290496e8c063112bc8
Taha Metwally <tmetwally5@gmail.com>
2024-06-08 09:07:12 -0500

    add documentation for the networ

--- Services/MyAPI.swift
+++ Services/MyAPI.swift
@@ -4,9 +4,15 @@
 //
 //  Created by Taha Metwally on 6/6/2024.
 //
+
 import Combine
 import Moya
 
+/**
+ The `MyAPI` enum defines different endpoints and their associated parameters for making requests to the meal database API (https://www.themealdb.com/api/json/v1/1/).
+ 
+ Each case in the enum represents a specific API endpoint along with the required parameters for that endpoint.
+ */
 enum MyAPI {
     case fetchMealDetailsByID(mealID: String)
     case fetchMealListByCategory(category: String)
@@ -17,10 +23,12 @@ enum MyAPI {
 }
 
 extension MyAPI: TargetType {
+    /// The base URL for all API requests.
     var baseURL: URL {
         return URL(string: "https://www.themealdb.com/api/json/v1/1/")!
     }
     
+    /// The path component of the URL for each API request.
     var path: String {
         switch self {
         case .fetchMealDetailsByID:
@@ -38,10 +46,12 @@ extension MyAPI: TargetType {
         }
     }
     
+    /// The HTTP method used for each API request.
     var method: Moya.Method {
         return .get
     }
     
+    /// The task to be performed for each API request, including parameters and encoding.
     var task: Task {
         switch self {
         case .fetchMealDetailsByID(let mealID):
@@ -59,10 +69,12 @@ extension MyAPI: TargetType {
         }
     }
     
+    /// The HTTP headers for each API request.
     var headers: [String: String]? {
         return ["Content-Type": "application/json"]
     }
     
+    /// Sample data to be used for testing purposes.
     var sampleData: Data {
         return Data()
     }
