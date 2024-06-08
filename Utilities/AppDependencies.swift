a5f9a0d2a89ed6931c313823002107511831b659
Taha Metwally <tmetwally5@gmail.com>
2024-06-08 08:49:17 -0500

    make the documentatino for appDependenceis and reachability and string+localization file

--- Utilities/AppDependencies.swift
+++ Utilities/AppDependencies.swift
@@ -5,26 +5,55 @@
 //  Created by Taha Metwally on 7/6/2024.
 //
 
-
 import SwiftUI
 import Moya
 import Network
 
+/**
+ A class that manages the dependencies of the FetchRewards application, making it easier to inject and manage dependencies throughout the app.
+
+ - Note: This class conforms to `ObservableObject`, which means it can be observed for changes in its published properties.
+ */
 class AppDependencies: ObservableObject {
 
+    /// A published property that provides the `MealDetailViewModel` instance.
     @Published var mealDetailViewModel: MealDetailViewModel
-    @Published var mealsViewModel: MealsViewModel
-    @Published var reachability: Reachability
-    private let networkService:NetworkService
-    private let moyaProvider:MoyaProvider<MyAPI>
-    private let  monitor: NWPathMonitor
 
+    /// A published property that provides the `MealsViewModel` instance.
+    @Published var mealsViewModel: MealsViewModel
+
+    /// A published property that provides the `Reachability` instance for monitoring network connectivity.
+    @Published var reachability: Reachability
+
+    /// The `NetworkService` instance used to manage network requests.
+    private let networkService: NetworkService
+
+    /// The `MoyaProvider` instance used to handle API requests.
+    private let moyaProvider: MoyaProvider<MyAPI>
+
+    /// The `NWPathMonitor` instance used to monitor network path changes.
+    private let monitor: NWPathMonitor
+
+    /**
+     Initializes a new `AppDependencies` instance, setting up all necessary dependencies for the application.
+     */
     init() {
+        // Initialize MoyaProvider with the specified API
         self.moyaProvider = MoyaProvider<MyAPI>()
+        
+        // Initialize NetworkService with the MoyaProvider
         self.networkService = NetworkService(provider: moyaProvider)
+        
+        // Initialize MealDetailViewModel with the NetworkService
         self.mealDetailViewModel = MealDetailViewModel(networkService: networkService)
+        
+        // Initialize MealsViewModel with the NetworkService
         self.mealsViewModel = MealsViewModel(networkService: networkService)
+        
+        // Initialize NWPathMonitor for monitoring network connectivity
         self.monitor = NWPathMonitor()
+        
+        // Initialize Reachability with the NWPathMonitor
         self.reachability = Reachability(monitor: monitor)
     }
 }
