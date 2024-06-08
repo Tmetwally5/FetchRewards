680ff9096ff03024e89e293faa272a009d08dd8a
Taha Metwally <tmetwally5@gmail.com>
2024-06-08 09:03:16 -0500

    add documentation for viewModel

--- ViewModels/MealDetailViewModel.swift
+++ ViewModels/MealDetailViewModel.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 import Combine
 
+/**
+ The `MealDetailViewModel` class is a view model that manages the fetching and storage of detailed meal information.
+ 
+ This class conforms to the `ObservableObject` protocol, allowing SwiftUI views to observe its properties and update accordingly.
+ */
 class MealDetailViewModel: ObservableObject {
     
+    /// Published property for holding the details of a meal, which can be observed by SwiftUI views.
     @Published var meal: MealDetails? = nil
-    private var cancellables = Set<AnyCancellable>()
-    private var networkService:NetworkService
     
-    init(networkService:NetworkService) {
+    /// Set to hold any cancellable Combine publishers to manage memory and lifecycle.
+    private var cancellables = Set<AnyCancellable>()
+    
+    /// Instance of `NetworkService` used for network requests.
+    private var networkService: NetworkService
+    
+    /**
+     Initializes the view model with a given network service.
+     
+     - Parameter networkService: The network service to be used for fetching data.
+     */
+    init(networkService: NetworkService) {
         self.networkService = networkService
     }
     
+    /**
+     Fetches the details of a meal by its ID.
+     
+     The method sends a network request to fetch meal details and updates the `meal` property with the fetched data.
+     
+     - Parameter id: The ID of the meal to fetch details for.
+     */
     func fetchMealDetail(byId id: String) {
         networkService.fetchMealDetailsByID(mealID: id)
             .receive(on: DispatchQueue.main)
@@ -25,7 +47,7 @@ class MealDetailViewModel: ObservableObject {
                 if case let .failure(error) = completion {
                     print("Failed to fetch meal detail: \(error.localizedDescription)")
                 }
-            }, receiveValue: {[weak self] response in
+            }, receiveValue: { [weak self] response in
                 self?.meal = response.meals.first
             })
             .store(in: &cancellables)
